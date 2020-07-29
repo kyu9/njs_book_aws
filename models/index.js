8e994cf293f3f760bff69f372f317a50437ea468
kyu9 <kyudo97@naver.com>
2020-07-29 14:07:58 +0900

    gitignore

--- models/index.js
+++ models/index.js
@@ -2,7 +2,7 @@
 const path = require('path');
 const Sequelize = require('sequelize');
 
-const env = process.env.NODE_ENV || 'production';
+const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
