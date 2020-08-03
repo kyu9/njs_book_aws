3b815bbbc54416188a8a38562aa1c4a968fde06d
kyu9 <kyudo97@naver.com>
2020-08-03 19:07:28 +0900

    adding...

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,5 @@
 var express = require('express');
 const redis = require('redis');
-const redisClient = redis.createClient();
 var User = require('../models').User;
 var router = express.Router();
 
